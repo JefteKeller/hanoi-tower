6d73f01f50aad526ba02f2f044c0d6422aaa3279
christian <csequeira153@gmail.com>
2020-09-15 16:43:47 -0300

    js done

--- script.js
+++ script.js
@@ -1,14 +1,57 @@
+let stage = null;
+let ultimoElemento = null;
+let ultimoPai = null;
+
 let firstTower = document.getElementById("firstTower");
-firstTower.addEventListener("click", checker(firstTower));
+firstTower.addEventListener("click", checker);
 
 let secondTower = document.getElementById("secondTower");
-secondTower.addEventListener("click", checker(secondTower));
+secondTower.addEventListener("click", checker);
 
 let thirdTower = document.getElementById("thirdTower");
-thirdTower.addEventListener("click", checker(thirdTower));
+thirdTower.addEventListener("click", checker);
 
-
-function checker(clickedTower) {
-  firstClick = clickedTower.lastElementChild;
-  
+function checker(e) {
+  let currentTower = e.currentTarget;
+  if (stage === 1) {
+    drop(currentTower);
+  } else {
+    get(currentTower);
+  }
 }
+
+function get(currentTower) {
+  ultimoElemento = currentTower.lastElementChild;
+  ultimoPai = currentTower;
+
+  if (ultimoElemento !== null) {
+    ultimoElemento.style.backgroundColor = "green";
+    stage = 1;
+  }
+}
+
+function drop(currentTower) {
+  ultimoElemento.style.backgroundColor = "white";
+
+  if (currentTower.lastElementChild === null) {
+    currentTower.appendChild(ultimoElemento);
+  } else if (
+    currentTower.lastElementChild.textContent > ultimoElemento.textContent
+  ) {
+    currentTower.appendChild(ultimoElemento);
+  } else if (
+    currentTower.lastElementChild.textContent < ultimoElemento.textContent
+  ) {
+    ultimoPai.appendChild(ultimoElemento);
+
+  }
+  vitoria()
+  stage = null;
+}
+
+function vitoria () {
+  if (thirdTower.childElementCount === 3) {
+    alert("voce ganhou")
+  }
+}
+
