396bb4600600a112accd44884fdc38b5382d57d1
Jefte Keller <kellergnr@gmail.com>
2020-09-17 02:49:27 -0300

    Improve JS and CSS

--- script.js
+++ script.js
@@ -1,15 +1,57 @@
 let stage = null;
+let count = null;
+let hasDisks = null;
 let ultimoElemento = null;
 let ultimoPai = null;
 
-let firstTower = document.getElementById("firstTower");
+let countResult = document.getElementById("count");
+let numberOfDisks = document.getElementById("numberOfDisks").value;
+
+const startButton = document.getElementById("startDisks");
+startButton.addEventListener("click", generateDisks);
+
+const firstTower = document.getElementById("firstTower");
 firstTower.addEventListener("click", checker);
 
-let secondTower = document.getElementById("secondTower");
+const secondTower = document.getElementById("secondTower");
 secondTower.addEventListener("click", checker);
 
-let thirdTower = document.getElementById("thirdTower");
+const thirdTower = document.getElementById("thirdTower");
 thirdTower.addEventListener("click", checker);
+
+function generateDisks() {
+  numberOfDisks = document.getElementById("numberOfDisks").value;
+  if (hasDisks == null) {
+    createDisks(numberOfDisks);
+    hasDisks = 1;
+  } else if (hasDisks == 1) {
+    reset();
+    createDisks(numberOfDisks);
+  }
+}
+
+function createDisks(numberOfDisks) {
+  const docFrag = document.createDocumentFragment();
+
+  for (let i = 1; i <= numberOfDisks; i++) {
+    const div = document.createElement("div");
+    div.classList.add("disks");
+    div.id = `disk${i}`;
+    docFrag.appendChild(div);
+  }
+  firstTower.appendChild(docFrag);
+}
+
+function reset() {
+  firstTower.innerHTML = "";
+  secondTower.innerHTML = "";
+  thirdTower.innerHTML = "";
+
+  document.getElementById("vitoria").classList.add("hidden");
+
+  countResult.textContent = 0;
+  count = 0;
+}
 
 function checker(e) {
   let currentTower = e.currentTarget;
@@ -26,32 +68,29 @@ function get(currentTower) {
 
   if (ultimoElemento !== null) {
     ultimoElemento.style.backgroundColor = "green";
-    ultimoElemento.style.color = "green";
     stage = 1;
   }
 }
 
 function drop(currentTower) {
   ultimoElemento.style.backgroundColor = "";
-  ultimoElemento.style.color = "";
 
   if (currentTower.lastElementChild === null) {
     currentTower.appendChild(ultimoElemento);
   } else if (
-    currentTower.lastElementChild.textContent > ultimoElemento.textContent
+    currentTower.lastElementChild.clientWidth > ultimoElemento.clientWidth
   ) {
     currentTower.appendChild(ultimoElemento);
   } else if (
-    currentTower.lastElementChild.textContent < ultimoElemento.textContent
+    currentTower.lastElementChild.clientWidth < ultimoElemento.clientWidth
   ) {
     ultimoPai.appendChild(ultimoElemento);
   }
-  vitoria();
-  stage = null;
-}
-
-function vitoria() {
-  if (thirdTower.childElementCount === 4) {
+  // Testa a condição de Vitória //
+  if (thirdTower.childElementCount == numberOfDisks) {
     document.getElementById("vitoria").classList.remove("hidden");
   }
+  stage = null;
+  count++;
+  countResult.textContent = count;
 }
